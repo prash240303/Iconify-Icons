8e90aef768ea03b0a918af65432bbb328e0db160
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-06-17 10:07:45 +0530

    modified:   src/App.css : changed hero title gradient     added footer text 	modified:   src/App.jsx 	modified:   src/components/Footer.css created a footer component 	modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,13 @@ import React from "react";
 export default function Footer()
 {
     return(
-        <footer class="footer-container">
-    
+        
+
+
     <div class="social-links">
       <div class="link"><a href="#"><i class="fab fa-facebook"></i></a></div>
       <div class="link"><a href="#"><i class="fab fa-instagram"></i></a></div>
       <div class="link"><a href="#"><i class="fab fa-twitter"></i></a></div>
     </div>
-    <div class="copyright">
-      <p>&copy; Copyright, 2023 - NB</p>
-    </div>
-  </footer>
     )
 }
